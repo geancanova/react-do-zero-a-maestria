5f4c72deb93210d21d1bd7d1bc8f222b7edff201
Geancarlo Canova <geancanovawt@gmail.com>
2023-09-15 15:40:40 -0300

    ADD: partial reactgram project

---
+++ 12_REACTGRAM/backend/routes/UserRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+// controller
+const { register, login, getCurrentUser } = require('../controllers/UserController');
+
+// middlewares
+const validate = require('../middlewares/handleValidation');
+const {
+  userCreateValidation,
+  loginValidation
+} = require('../middlewares/userValidations');
+const authGuard = require('../middlewares/authGuard');
+
+// routes
+router.post('/register', userCreateValidation(), validate, register);
+router.post('/login', loginValidation(), validate, login);
+router.get('/profile', authGuard, getCurrentUser)
+
+module.exports = router;
