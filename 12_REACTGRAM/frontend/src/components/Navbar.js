fd75b9a58bd684fa7e8a18147efca368fa9d4b7f
geancanova <geancanovawt@gmail.com>
2023-12-01 16:25:55 -0300

    feat: update pages and components

---
+++ 12_REACTGRAM/frontend/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import './Navbar.css';
+
+// Components
+import { NavLink, Link } from 'react-router-dom';
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav id='nav'>
+        <Link to={"/"}>ReactGram</Link>
+        <form id="search-form">
+            <BsSearch />
+            <input type="text" placeholder='Pesquisar...' />
+        </form>
+        <ul id="nav-links">
+            <li>
+                <NavLink to={"/"}>
+                    <BsHouseDoorFill />
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to={"/login"}>
+                    <BsFillPersonFill />
+                    Entrar
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to={"/register"}>
+                    <BsFillCameraFill />
+                    Cadastrar
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
