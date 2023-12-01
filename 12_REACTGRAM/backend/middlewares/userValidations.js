410a290c063d7a54e7cf82259a4de47ce67c32ad
geancanova <geancanovawt@gmail.com>
2023-12-01 16:20:24 -0300

    fix: backend user validation

--- 12_REACTGRAM/backend/middlewares/userValidations.js
+++ 12_REACTGRAM/backend/middlewares/userValidations.js
@@ -21,7 +21,7 @@ const userCreateValidation = () => {
         min: 5
       })
       .withMessage('A senha deve ter no mínimo 5 caracteres.'),
-    body('confirmpassword')
+    body('confirmPassword')
       .isString()
       .withMessage('A confirmação de senha é obrigatória.')
       .custom((value, { req }) => {
