1d72df9225a08fc7f46fe19ceb757549461e76a9
geancanova <geancanovawt@gmail.com>
2023-12-05 15:17:55 -0300

    feat: init profile page

---
+++ 12_REACTGRAM/frontend/src/pages/Profile/Profile.js
@@ -0,0 +1,20 @@
+import { uploads } from "../../utils/config";
+
+// Components
+import { Link } from "react-router-dom";
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs";
+
+// Hooks
+import { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
