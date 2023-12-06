057fedf677d5b672caf861aac045d992db3d5c4f
geancanova <geancanovawt@gmail.com>
2023-12-06 16:21:23 -0300

    feat: add Photo page and like component

--- 12_REACTGRAM/frontend/src/pages/Photo/Photo.js
+++ 12_REACTGRAM/frontend/src/pages/Photo/Photo.js
@@ -5,6 +5,8 @@ import { uploads } from '../../utils/config';
 // Components
 import Message from '../../components/Message';
 import { Link } from 'react-router-dom';
+import PhotoItem from '../../components/PhotoItem';
+import LikeContainer from '../../components/LikeContainer';
 
 // Hooks
 import { useEffect, useState } from 'react';
@@ -12,11 +14,42 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 // Redux
-
+import { 
+  getPhoto,
+  likePhoto
+} from '../../slices/photoSlice';
 
 const Photo = () => {
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+  const { photo, loading, error, message } = useSelector((state) => state.photo);
+
+  // Comments
+
+  // Load photo data
+  useEffect(() => {
+    dispatch(getPhoto(id));
+  }, [dispatch, id]);
+
+  // Like and Comments
+  const handleLike = (photo) => {
+    return () => {
+      dispatch(likePhoto(photo._id));
+    }
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
+
   return (
-    <div>Photo</div>
+    <div id="photo">
+      <PhotoItem photo={photo} />
+      <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+    </div>
   )
 }
 
--- 12_REACTGRAM/frontend/src/services/photoService.js
+++ 12_REACTGRAM/frontend/src/services/photoService.js
@@ -60,11 +60,43 @@ const updatePhoto = async (data, id, token) => {
   }
 };
 
+// Get photo by id
+const getPhoto = async (id, token) => {
+  const config = requestConfig("GET", null, token);
+
+  try {
+    const res = await fetch(`${api}/photos/${id}`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Like a photo
+const likePhoto = async (id, token) => {
+  const config = requestConfig("PUT", null, token);
+
+  try {
+    const res = await fetch(`${api}/photos/like/${id}`, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
   deletePhoto,
   updatePhoto,
+  getPhoto,
+  likePhoto
 };
 
 export default photoService;
