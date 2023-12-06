0dd2dec73dd85cd098f50b0c806c35bc7717f541
geancanova <geancanovawt@gmail.com>
2023-12-06 14:32:43 -0300

    feat: create Photo page

--- 12_REACTGRAM/frontend/src/App.js
+++ 12_REACTGRAM/frontend/src/App.js
@@ -12,6 +12,7 @@ import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import EditProfile from './pages/EditProfile/EditProfile';
 import Profile from './pages/Profile/Profile';
+import Photo from './pages/Photo/Photo';
 
 // Components
 import Navbar from './components/Navbar';
@@ -49,6 +50,10 @@ function App() {
             <Route
               path="/register"
               element={!auth ? <Register /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/photos/:id"
+              element={auth ? <Photo /> : <Navigate to="/login" />}
             />
           </Routes>
         </div>
